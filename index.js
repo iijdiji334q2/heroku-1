7a72774cb79ffec19a6579b12f86777604a89219
stargazer39 <dehemiweerakkodi2@gmail.com>
2021-03-03 21:43:56 +0530

    commit

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ var httpSrv = http.createServer((req,res)=>{
 	//res.setHeader("Access-Control-Allow-Origin",'*');
     res.writeHead(200,{'Content-Type' : 'text/plain'});
     console.log(URL.parse(req.url,true));
-	var ip = req.headers['x-forwarded-for'] || 
-			     req.connection.remoteAddress || 
-			     req.socket.remoteAddress ||
-			     (req.connection.socket ? req.connection.socket.remoteAddress : null);
+	var ip = "heroku-for-me.herokuapp.com";
 	try{
 		ping.sys.probe(ip, function(isAlive){
 	        var msg = isAlive ? 'host ' + ip + ' is alive' : 'host ' + ip + ' is dead';
